2ba8ae46946dd88fe153e7566b0d4579d3026b16
AndreasJepsenNielsen <andr921c@stud.kea.dk>
2018-05-21 09:40:55 +0200

    Rettelser

--- src/main/java/com/example/demo/Repository/EventRepository.java
+++ src/main/java/com/example/demo/Repository/EventRepository.java
@@ -48,7 +48,6 @@ public class EventRepository implements EventInterface {
         while (rs.next())
         {
          listOfArrears.add(new Relative(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5)));
-            System.out.println("FOund Oliver");
         }
 
 
